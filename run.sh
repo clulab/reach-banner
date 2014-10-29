8272675a373e214bd1df8e0622dc6b0cd710cb45
Mihai Surdeanu <surdeanu@gmail.com>
2014-10-29 16:16:03 -0700

    Cleaned the run.sh script

--- run.sh
+++ run.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+BC2=/Users/mihais/corpora/bc2geneMention # change this to the location of the BioCreative 2 dataset!
+TRAIN=$BC2/train/train.in
+GENE=$BC2/train/GENE.eval
+ALTGENE=$BC2/train/ALTGENE.eval
+MODEL=banner_data/banner_model.dat
+PROPORTION=0.01
+
+CLASSPATH=target/scala-2.10/classes/:lib/dragontool.jar:lib/heptag.jar
+
 mkdir -p tmp
-#./train.sh ../bc2geneMention/train/train.in ../bc2geneMention/train/GENE.eval banner_model.dat 0.01
-./test.sh ../bc2geneMention/train/train.in ../bc2geneMention/train/GENE.eval ../bc2geneMention/train/ALTGENE.eval banner_model.dat tmp
+
+# Train (take out $PROPORTION to train on the whole dataset!)
+java -Xmx4G -Xms4G -cp $CLASSPATH bc2.TrainModel banner_data/banner.properties $TRAIN $GENE $ALTGENE $MODEL $PROPORTION
+
+# Test 
+java -Xmx4G -Xms4G -cp $CLASSPATH bc2.TestModel banner_data/banner.properties $TRAIN $GENE $ALTGENE $MODEL tmp 
